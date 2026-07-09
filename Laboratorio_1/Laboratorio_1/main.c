2940c5c53f2bf57cb65293808236fb779fe8cafd
car3720 <157552024+car3720@users.noreply.github.com>
2026-07-09 17:09:28 -0600

    Add files via upload

---
+++ Laboratorio_1/Laboratorio_1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Laboratorio_1.c
+ *
+ * Created: 09/07/2026 17:05:10
+ * Author : Diego Cardona
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
